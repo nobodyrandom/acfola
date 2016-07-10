202d71aa9e906a2d2f7e71eb417fe1ebd657cca2
Nobody Random <iamnobodyrandom@yahoo.com>
2016-07-10 00:56:29 -0700

    init

---
+++ public_html/links.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrisonchow
+ * Date: 7/10/16
+ * Time: 12:52 AM
+ */
+
+$linkTo = $_GET['link'];
+switch ($linkTo) {
+    case "facebook":
+        $url = "https://www.facebook.com/";
+        break;
+    case "blog":
+    case "rss":
+    case "medium":
+        $url = "https://medium.com/";
+        break;
+    case "github":
+        $url = "https://github.com/";
+        break;
+    case "home":
+    default:
+        $url = "https://developersfoundation.ca";
+}
+
+header("Location: " . $url);
+exit;
+?>
